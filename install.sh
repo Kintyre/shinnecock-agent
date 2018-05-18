71bcc4f3a7fe2620d90efce7ade9271d073d8fdc
Lowell Alleman <lowell@kintyre.co>
2018-05-18 18:37:11 -0400

    Fix for running on windows

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@
 
 virtualenv venv || exit 1
 # Upgrade pip (ssl issue); shouldn't be needed long-term
-. venv/bin/activate || exit 1
 
+if [[ -f venv/bin/activate ]]
+then
+    . venv/bin/activate || exit 1
+else # Try windows (mysys/cygwin)
+    . venv/Scripts/activate || exit 1
+fi
 #pip install -r requirements.txt
 
 pip install .
